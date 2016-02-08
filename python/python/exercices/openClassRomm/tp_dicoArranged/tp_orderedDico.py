702d427057b2f50a2419990a2a3c1c27666fe92b
hamzas <skander.hamza@st.com>
2016-02-08 20:32:05 +0100

    ex tp_dico _add_ items()

--- python/python/exercices/openClassRomm/tp_dicoArranged/tp_orderedDico.py
+++ python/python/exercices/openClassRomm/tp_dicoArranged/tp_orderedDico.py
@@ -52,11 +52,42 @@ class ArrangedDico:
             sortedVal.append(self.dicoValues[idx])
         self.dicoKey = copy.copy(sortedKey)
         self.dicoValues = copy.copy(sortedVal)
+    def __iter__(self):
+        self.current = 0
+        return self
+    def __next__(self):
+        if (self.current+1 > len(self.dicoValues)):
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.dicoKey[self.current-1], self.dicoValues[self.current-1]
+    def keys(self):
+        return self.dicoKey
+    def values(self):
+        return self.dicoValues
+
+    def items(self):
+        for i, key in enumerate(self.dicoKey):
+            valeur = self.dicoValues[i]
+            yield (key,valeur)
+    def __add__(self,newDico):
+        if ( type(newDico) is not type (self)):
+            raise TypeError ("Impossible d'ajouter {0} & {1}".format(\
+                    type(self),type(newDico)))
+        else:
+            nouveau=ArrangedDico()
+            for key,val in self.items():
+                nouveau[key]=val
+            for key,val in newDico.items():
+                nouveau[key]=val
+        return nouveau
 
 if __name__ == '__main__':
     legumes= {"oranges":22,"fraise":10}
+    fruits={"patates":3,"tomates":8}
     monDico = ArrangedDico(oranges=25, pommes=32)
     monDico = ArrangedDico(**legumes)
+    NewDico = ArrangedDico(**fruits)
 
     print (monDico)
     del monDico["oranges"]
@@ -83,3 +114,14 @@ if __name__ == '__main__':
     print (" *** reverse: ")
     monDico.reverse()
     print (monDico)
+    for i,j in monDico:
+        print (i,j)
+    for key in monDico.keys():
+        print (key)
+    for val in monDico.values():
+        print (val)
+    monDico = monDico + NewDico
+    print (monDico)
+    for key,val in monDico.items():
+        print ("using item {} {}".format(key,val))
+
