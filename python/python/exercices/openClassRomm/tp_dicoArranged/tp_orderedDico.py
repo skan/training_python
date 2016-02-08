5644b51148f015c83308f62fb9622a67a158e6ff
hamzas <skander.hamza@st.com>
2016-02-08 16:30:04 +0100

    ex tp_dico: __str__

--- python/python/exercices/openClassRomm/tp_dicoArranged/tp_orderedDico.py
+++ python/python/exercices/openClassRomm/tp_dicoArranged/tp_orderedDico.py
@@ -5,10 +5,24 @@ class ArrangedDico:
         self.dicoValues = []
         if (articles is not None):
             for key, value in articles.items():
-                print (key,value)
                 self.dicoKey.append(key)
                 self.dicoValues.append(value) 
+    def __str__(self):
+        ret_str = "{"
+        lengh = len (self.dicoValues)
+        for i in range (lengh):
+            ret_str+= "'{}':{},".format(self.dicoKey[i],self.dicoValues[i]) 
+        ret_str = ret_str[:-1]
+        ret_str+= "}"
+        return ret_str
+    def __delitem__(self,value):
+       idx = self.dicoKey.index(value)
+        
 legumes= {"oranges":22,"fraise":10}
 monDico = ArrangedDico(oranges=25, pommes=32)
 monDico = ArrangedDico(**legumes)
+
+print (monDico)
+del monDico["oranges"]
+print (monDico)
 
